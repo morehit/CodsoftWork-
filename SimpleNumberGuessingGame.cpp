30dbda497cceba8a778d4a90ecca50dbe0602164
morehit <95517329+morehit@users.noreply.github.com>
2023-10-17 21:41:43 +0530

    Create SimpleNumberGuessingGame.cpp

---
+++ SimpleNumberGuessingGame.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib> // for rand() and srand() functions
+#include <ctime>   // for time() function
+
+int main() {
+    // Seed the random number generator
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    // Generate a random number between 1 and 100
+    int secretNumber = std::rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+
+    std::cout << "Welcome to the Number Guessing Game!" << std::endl;
+
+    do {
+        std::cout << "Guess the number (between 1 and 100): ";
+        std::cin >> guess;
+        attempts++;
+
+        if (guess < secretNumber) {
+            std::cout << "Too low! Try again." << std::endl;
+        } else if (guess > secretNumber) {
+            std::cout << "Too high! Try again." << std::endl;
+        } else {
+            std::cout << "Congratulations! You guessed the number " << secretNumber << " in " << attempts << " attempts." << std::endl;
+        }
+
+    } while (guess != secretNumber);
+
+    return 0;
+}
